aec9b8c0f08a1bf55b5e4e601188f68ecb81dc02
Thales Menzner <thalesmenzner@gmail.com>
2022-11-20 18:22:50 -0300

    to do list

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function adicionar() {
+    const input = document.getElementById("input")
     const tarefaPura = document.getElementById("input").value
     const listaDeTarefas = document.getElementById('listaDeTarefas')
 
@@ -6,19 +7,29 @@ function adicionar() {
     const tarefa = document.createTextNode(` ${tarefaPura}`)
     const checkbox = document.createElement('input')
     
-    checkbox.type = "checkbox";
-    checkbox.name = "checkName";
-    checkbox.id = "idCheck";
-    checkbox.value = 'valor'
-    checkbox.checked = false
- 
-    checkbox.addEventListener('click', function checkOff() {
-        console.log('desativando')
-    })
+    if(input.value != '') {
+        checkbox.type = "checkbox";
+        checkbox.name = "checkName";
+        checkbox.id = "idCheck";
+        checkbox.value = 'valor'
+        checkbox.checked = false
 
-    paragrafo.appendChild(checkbox)
-    paragrafo.appendChild(tarefa)
-    listaDeTarefas.appendChild(paragrafo)
+
+        paragrafo.appendChild(checkbox)
+        paragrafo.appendChild(tarefa)
+        listaDeTarefas.appendChild(paragrafo)
+
+        input.value = ''
+        
+
+        checkbox.addEventListener('click', function checkOff() {
+            if(checkbox.checked == true) {
+                paragrafo.style.textDecoration = 'line-through'
+            } else if (checkbox.checked == false) {
+                paragrafo.style.textDecoration = 'none'
+            } 
+        })
+    }
 }
 
 
